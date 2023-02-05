6697501e66d9c493c0bc4bbd195efa7848e03a78
VINH PHUC LE <vincentle@VINHs-MacBook-Pro.local>
2023-02-05 19:17:55 +0700

    create new address and privateKey

--- scripts/createWallet.ts
+++ scripts/createWallet.ts
@@ -1,13 +1,25 @@
 // import { ethers } from "hardhat";
+var Accounts = require('web3-eth-accounts');
+
+const JSONRPC_URL = process.env.JSONRPC_URL ?? "";
+const accounts = new Accounts(JSONRPC_URL)
 
 async function main() {
+    const data = await accounts.create();
 
-  console.log("Create Account", "Account", "privatekey");
+    console.log(data);
+    
+    console.log("Account Created");
+    console.log("address", data.address);
+    console.log("privateKey", data.privateKey);
+    // console.log("signTransaction", data.signTransaction);
+    // console.log("sign", data.sign);
+    // console.log("encrypt", data.encrypt);
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
