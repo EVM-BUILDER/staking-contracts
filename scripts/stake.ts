ffae534b136cf1eb4faff20661463e1249875f78
XbuildApp <vinhphuc591@gmail.com>
2023-02-23 17:05:57 +0700

    Update stake.ts

--- scripts/stake.ts
+++ scripts/stake.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 import { Staking } from "../types/Staking";
 
 const STAKING_CONTRACT_ADDRESS = process.env.STAKING_CONTRACT_ADDRESS ?? "";
-const STAKE_AMOUNT = ethers.utils.parseEther("100000");
+const STAKE_AMOUNT = ethers.utils.parseEther("10000");
 
 async function main() {
   const [account] = await ethers.getSigners();
